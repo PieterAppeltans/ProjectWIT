a5bd5678d66da77aa1d232e7c0a7e7e9964f6169
PieterAppeltans <pieter@appeltans.eu>
2017-02-22 11:29:22 +0100

    changes

--- Code/fem.cpp
+++ Code/fem.cpp
@@ -14,14 +14,13 @@ int main(){
 
 void f(){
   }
-void A(Triangle triangles[],int nb_triangles){
-  boost::numeric::ublas::mapped_matrix<double> matrix;
-  boost::numeric::ublas::mapped_matrix<double> M;
+void A(Triangle triangles[],int nb_triangles,boost::numeric::ublas::mapped_matrix<double> A){
+  boost::numeric::ublas::matrix<double> M (3,3);
   for (int i=0;i<nb_triangles;i++){
     local(triangles[i],M);
     for(int j= 0;j<3;j++){
       for(int k =0;k<3;k++){
-        matrix(triangles[i].node(j).id(),triangles[i].node(k).id()) += M(j,k);
+        A(triangles[i].node(j).id(),triangles[i].node(k).id()) += M(j,k);
       }
     }
   }
--- Code/node.hpp
+++ Code/node.hpp
@@ -2,21 +2,23 @@
 #define NODE_HPP 23
 class Node{
 public:
+  Node(){};
   Node(int id,double r,double z)
   :id_(id),r_(r),z_(z)
   {};
   Node(Node const& node)
   :id_(node.id_),r_(node.r_),z_(node.z_)
   {};
+  int id(){
+    return id_;
+  }
   void operator= (Node const& n)
   {
     id_= n.id_;
     r_ = n.r_;
     z_ = n.z_;
   }
-  int id(){
-    return id_;
-  }
+
 private:
   int id_;
   double r_;
