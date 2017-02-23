3a27bcfa0f055b23e31a8266cdd76405a51f691b
lennartbu <lennart.bulteel@student.kuleuven.be>
2017-02-23 22:53:44 +0100

    vertices and triangles corrected, area added to triangles

--- code/fem.cpp
+++ code/fem.cpp
@@ -13,32 +13,19 @@ using namespace boost::numeric::ublas;
 /* COMPILE WITH: g++ -Wall -std=c++14 -O3 -lstdc++ -o fem fem.cpp
 mesh_reader.hpp needs those two flags for reading files */
 
-void A(Triangle triangles[], int nb_triangles);
-void  f();
-void local(Triangle triangle, matrix<double> M);
-
 int main(){
-    matrix<double> triangles = mesh::read_triangles();
+    /* Get node coordinates, boundary flag, triangle node id's, triangle areas */
     matrix<double> vertices = mesh::read_vertices();
-    std::cout << triangles << std::endl;
-    std::cout << vertices << std::endl;
+    matrix<double> triangles = mesh::read_triangles(vertices);
+    // std::cout << triangles << std::endl;
+
+    /* Define constants */
+
+    /* Calculate righthand side vector components */
+
+    /* Calculate stiffness matrix */
+
+    /* Calculate Jacobian */
+
     return 0;
-}
-
-void f(){
-  }
-void A(Triangle triangles[],int nb_triangles, mapped_matrix<double> A){
-  matrix<double> M (3,3);
-  for (int i=0;i<nb_triangles;i++){
-    local(triangles[i],M);
-    for(int j= 0;j<3;j++){
-      for(int k =0;k<3;k++){
-        A(triangles[i].node(j).id(),triangles[i].node(k).id()) += M(j,k);
-      }
-    }
-  }
-}
-
-void local(Triangle triangle, matrix<double> M){
-
 }
--- code/mesh_reader.hpp
+++ code/mesh_reader.hpp
@@ -3,7 +3,9 @@
 
 #include <fstream>
 #include <sstream>
+#include <iostream>
 #include <string>
+#include <math.h>
 #include <boost/numeric/ublas/matrix.hpp>
 #include <boost/numeric/ublas/io.hpp>
 
@@ -11,42 +13,13 @@ using namespace boost::numeric::ublas;
 
 namespace mesh {
 
-    matrix<double> read_triangles() {
-        /* index # is triangle id, contains
-        the 3 vertix id's of the corner nodes */
-        std::ifstream infile("../triangle/pear.1.ele");
-        std::string line;
-        int a, b, c, d;
-        int line_nb = 0;
-        std::getline(infile, line);
-        std::istringstream iss(line);
-        iss >> a >> b >> c;
-        int nb_triangles = a;
-        matrix<double> triangles (nb_triangles, 3);
-
-        while (std::getline(infile, line)) {
-            std::istringstream iss(line);
-            if (iss >> a >> b >> c >> d) {
-                triangles(line_nb,0) = b;
-                triangles(line_nb,1) = c;
-                triangles(line_nb,2) = d;
-            }
-            else {
-                break;
-            }
-            line_nb += 1;
-        }
-
-        return triangles;
-    }
-
     matrix<double> read_vertices() {
         /* index # is vertix id, contains x and y
         coordinate respectively of vertix, and
         a flag, 1 if boundary point 0 if not */
         std::ifstream infile("../triangle/pear.1.node");
         std::string line;
-        int a, b, c, d;
+        double a, b, c, d;
         int line_nb = 0;
         std::getline(infile, line);
         std::istringstream iss(line);
@@ -57,9 +30,9 @@ namespace mesh {
         while (std::getline(infile, line)) {
             std::istringstream iss(line);
             if (iss >> a >> b >> c >> d) {
-                vertices(line_nb,0) = b;
-                vertices(line_nb,1) = c;
-                vertices(line_nb,2) = d;
+                vertices(line_nb, 0) = b;
+                vertices(line_nb, 1) = c;
+                vertices(line_nb, 2) = d;
             }
             else {
                 break;
@@ -68,6 +41,40 @@ namespace mesh {
         }
 
         return vertices;
+    }
+
+    matrix<double> read_triangles(matrix<double> &vertices) {
+        /* index # is triangle id, contains
+        the 3 vertix id's of the corner nodes  and
+        the area of the triangle, calculated using
+        the vertices matrix */
+        std::ifstream infile("../triangle/pear.1.ele");
+        std::string line;
+        int a, b, c, d;
+        int line_nb = 0;
+        std::getline(infile, line);
+        std::istringstream iss(line);
+        iss >> a >> b >> c;
+        int nb_triangles = a;
+        matrix<double> triangles (nb_triangles, 4);
+
+        while (std::getline(infile, line)) {
+            std::istringstream iss(line);
+            if (iss >> a >> b >> c >> d) {
+                triangles(line_nb, 0) = b-1;
+                triangles(line_nb, 1) = c-1;
+                triangles(line_nb, 2) = d-1;
+                triangles(line_nb, 3) = 0.5*fabs(vertices(b-1, 0)*(vertices(c-1, 1) - vertices(d-1, 1)) +
+                    vertices(c-1, 0)*(vertices(d-1, 1) - vertices(b-1, 1)) +
+                    vertices(d-1, 0)*(vertices(b-1, 1) - vertices(c-1, 1)));
+            }
+            else {
+                break;
+            }
+            line_nb += 1;
+        }
+
+        return triangles;
     }
 
 }
