de4c16d0623cb599c7373a4b7f723f99ffb4716c
PieterAppeltans <pieter@appeltans.eu>
2017-03-05 17:41:35 +0100

    C++ update

--- code/fem.cpp
+++ code/fem.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <math.h>
 #include <boost/numeric/ublas/vector.hpp>
+#include <boost/numeric/ublas/vector_proxy.hpp>
 #include <boost/numeric/ublas/io.hpp>
 #include <boost/numeric/ublas/matrix_sparse.hpp>
 #include <boost/numeric/ublas/matrix.hpp>
@@ -21,69 +22,75 @@ const double DU_R = 2.8*pow(10, -10);
 const double DU_Z = 1.1*pow(10, -9);
 const double DV_R = 2.32*pow(10, -9);
 const double DV_Z = 6.97*pow(10, -9);
+const double rq = 0.97;
 
 /* Global variables for applicable storage scenario */
 const double ETA_U = 0.208;
 const double ETA_V = 0.0004;
 const double T_CEL = 298.15;
 
-double Cvamb(double const &t) {
-  return (101300*ETA_U)/(8.314*t);
-}
+const double Cvamb = (101300*ETA_U)/(8.314*T_CEL);
+const double Cuamb = (101300*ETA_V)/(8.314*T_CEL);
+const double Vmfv = 1.61*pow(10, -4)*exp((56700/8.314)*(1/293.15 - 1/T_CEL));
+const double Vmu = 2.39*pow(10, -4)*exp((80200/8.314)*(1/293.15 - 1/T_CEL));
 
-double Cuamb(double const &t) {
-  return (101300*ETA_V)/(8.314*t);
-}
 
-double Vmfv(double const &t) {
-  return 1.61*pow(10, -4)*exp((56700/8.314)*(1/293.15 - 1/t));
-}
-
-double Vmu(double const &t) {
-  return 2.39*pow(10, -4)*exp((80200/8.314)*(1/293.15 - 1/t));
-}
-
-double Ru_simple(double &u, double &t) {
+double Ru_simple(double u) {
   /* To calculate starting value of u for solving
   nonlinear system of equations */
-  return Vmu(t)*u/0.4103;
+  return Vmu*u/0.4103;
 }
 
-double Rv_simple(double u, double t) {
+double Rv_simple(double u) {
   /* To calculate starting value of v for solving
   nonlinear system of equations */
-  return 0.97*Ru_simple(u, t) + Vmfv(t);
+  return rq*Ru_simple(u) + Vmfv;
 }
 
 class Ru {
 
-  private:
-    double t;
-
   public:
-    Ru(double ti) {
-      t = ti;
+    Ru() {
     }
-    double operator()(double u, double v) const {
-      return (Vmu(t)*u)/((0.4103+u)*(1+(v/27.2438)));
+    template vector<double,typename T> operator()(double u, double v) const {
+      return element_div(Vmu*u,element_prod(0.4103+u,1+(v/27.2438)));
     }
 
 };
 
 class Rv {
-
-  private:
-    double t;
-
   public:
-    Rv(double ti) {
-      t = ti;
-    }
-    double operator()(double u, double v) const {
-      Ru Ru_funct(t);
-      return 0.97*Ru_funct(u, v)+ Vmfv(t)/(1+(u/0.1149));
+    Rv() { }
+    vector<double> operator()(vector<double> u, vector<double> v) const {
+      Ru Ru_funct;
+      return 0.97*Ru_funct(u, v)+ element_div(Vmfv*scalar_vector(u.length(),1),1+(u/0.1149));
     }
 
+};
+class F{
+private:
+  matrix<double> Au_;
+  matrix<double> Av_;
+  matrix<double> B_;
+  matrix<double> C_;
+  vector<double> D_;
+  int N_;
+public:
+  F(matrix<double> Au,matrix<double> Av, matrix<double> B,matrix<double>C,vector<double>D,int N)
+  {
+    Au_ =Au;Av_ = Av; B_ = B;C_ = C;D_ =D;N_= N;
+  };
+  vector<double> operator()(vector<double> x){
+    vector<double> result(2*N_);
+    vector<double> temp(N_);
+    vector<double> u = project(x,range(0,N_));
+    vector<double> v = project(x,range(N_,2*N_));
+    temp = prod(Au_, u)+ prod(B_,Ru(u,v))+hu*(prod(C_,u)-D_*Cuamb);
+    project(result,range(0,N_)) = temp;
+    temp = -prod(Av_,v)+ prod(B_,Rv(u,v))-hv*(prod(C_,v)-D_*Cvamb);
+    project(result,range(N_,2*N_)) = temp;
+    return result;
+  };
 };
 
 int main() {
@@ -113,47 +120,59 @@ int main() {
 
   /* Calculate first part of stiffness matrix A */
   matrix<double> init_A(vertices.size1(), vertices.size1());
+  matrix<double> I_U(2,2);
+  matrix<double> I_V(2,2);
+  I_U(0,0) = DU_R;
+  I_U(1,1) = DU_Z;
+  I_V(0,0) = DV_R;
+  I_V(1,1) = DV_Z;
   matrix<double> G(3, 2);
-  matrix<double> GGT(3, 3);
-  symmetric_adaptor<matrix<double>, lower> A(init_A);
+  matrix<double> GGT_U(3, 3);
+  matrix<double> GGT_V(3, 3);
+  symmetric_adaptor<matrix<double>, lower> A_U(init_A);
+  symmetric_adaptor<matrix<double>, lower> A_V(init_A);
   for (unsigned t = 0; t < triangles.size1(); ++t) {
     int a = triangles(t, 0);
     int b = triangles(t, 1);
     int c = triangles(t, 2);
     double area = triangles(t, 3);
-    G(0, 0) = (vertices(b, 1) - vertices(c, 1))*sqrt(DU_R);
-    G(1, 0) = (vertices(c, 1) - vertices(a, 1))*sqrt(DU_R);
-    G(2, 0) = (vertices(a, 1) - vertices(b, 1))*sqrt(DU_R);
-    G(0, 1) = (vertices(c, 0) - vertices(b, 0))*sqrt(DU_Z);
-    G(1, 1) = (vertices(a, 0) - vertices(c, 0))*sqrt(DU_Z);
-    G(2, 1) = (vertices(b, 0) - vertices(a, 0))*sqrt(DU_Z);
-    GGT = (1/(2*area))*((vertices(a, 0)+vertices(b, 0)+vertices(c, 0))/6)*block_prod<matrix<double>, 64> (G, trans(G));
-    A(a, a) += GGT(0, 0);
-    A(b, a) += GGT(1, 0);
-    A(c, a) += GGT(2, 0);
-    A(b, b) += GGT(1, 1);
-    A(b, c) += GGT(1, 2);
-    A(c, c) += GGT(2, 2);
+    G(0, 0) = (vertices(b, 1) - vertices(c, 1));
+    G(1, 0) = (vertices(c, 1) - vertices(a, 1));
+    G(2, 0) = (vertices(a, 1) - vertices(b, 1));
+    G(0, 1) = (vertices(c, 0) - vertices(b, 0));
+    G(1, 1) = (vertices(a, 0) - vertices(c, 0));
+    G(2, 1) = (vertices(b, 0) - vertices(a, 0));
+    GGT_U = (1/(2*area))*((vertices(a, 0)+vertices(b, 0)+vertices(c, 0))/6)*block_prod<matrix<double>, 64> (block_prod<matrix<double>,64>(G,I_U), trans(G));
+    GGT_V = (1/(2*area))*((vertices(a, 0)+vertices(b, 0)+vertices(c, 0))/6)*block_prod<matrix<double>, 64> (block_prod<matrix<double>,64>(G,I_V), trans(G));
+    A_U(a, a) += GGT_U(0, 0);
+    A_U(b, a) += GGT_U(1, 0);
+    A_U(c, a) += GGT_U(2, 0);
+    A_U(b, b) += GGT_U(1, 1);
+    A_U(b, c) += GGT_U(1, 2);
+    A_U(c, c) += GGT_U(2, 2);
+
+    A_V(a, a) += GGT_V(0, 0);
+    A_V(b, a) += GGT_V(1, 0);
+    A_V(c, a) += GGT_V(2, 0);
+    A_V(b, b) += GGT_V(1, 1);
+    A_V(b, c) += GGT_V(1, 2);
+    A_V(c, c) += GGT_V(2, 2);
   }
 
   /* Boundary condition integrals: second part of A and a constant vector term */
-  vector<double> V(2);
   matrix<double> init_C(vertices.size1(), vertices.size1());
   symmetric_adaptor<matrix<double>, lower> C(init_C);
-  matrix<double> init_D(vertices.size1(), vertices.size1());
-  symmetric_adaptor<matrix<double>, lower> D(init_D);
+  vector<double> D(vertices.size1());
   for (unsigned b = 0; b < boundaries.size1(); ++b) {
     double len = sqrt(pow(vertices(boundaries(b, 0), 0) - vertices(boundaries(b, 1), 0), 2) +
       pow(vertices(boundaries(b, 0), 1) - vertices(boundaries(b, 1), 1), 2));
     C(boundaries(b, 0), boundaries(b, 0)) += len*(vertices(boundaries(b, 0), 0)/4 + vertices(boundaries(b, 1), 0)/12);
     C(boundaries(b, 0), boundaries(b, 1)) += len*(vertices(boundaries(b, 0), 0)/12 + vertices(boundaries(b, 1), 0)/12);
     C(boundaries(b, 1), boundaries(b, 1)) += len*(vertices(boundaries(b, 0), 0)/12 + vertices(boundaries(b, 1), 0)/4);
-    V(0) = vertices(boundaries(b, 0), 0)/3 + vertices(boundaries(b, 1), 0)/6;
-    V(1) = vertices(boundaries(b, 0), 0)/6 + vertices(boundaries(b, 1), 0)/3;
-    V *= len*Cuamb(T_CEL);
+    D(boundaries(b,0)) += len*(vertices(boundaries(b,0),0)/3.+vertices(boundaries(b,1),0)/6.);
+    D(boundaries(b,1)) += len*(vertices(boundaries(b,0),0)/6.+vertices(boundaries(b,1),0)/3.);
   }
 
-  /* KINSOL: solving nonlinear system numerically */
 
 
   return 0;
