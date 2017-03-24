911e83e0ffd48bc51457103fcc2ecf0ac6c6c1ae
PieterAppeltans <pieter@appeltans.eu>
2017-03-24 17:35:23 +0100

    Minor Tweaking

--- triangle/circle.py
+++ triangle/circle.py
@@ -2,7 +2,7 @@ import numpy as np
 count = 1
 print "250 2 0 1"
 for t in np.linspace(-np.pi/2,np.pi/2,250):
-	print count," ",0.05*np.cos(t)," ",0.05*np.sin(t)," ",1
+	print count," ",50*np.cos(t)," ",50*np.sin(t)," ",1
 	count += 1
 print "250 0 1"
 for i in range(1,250):
