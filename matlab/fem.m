edc56d2dca9af05486cf84466976750d1e6ed092
PieterAppeltans <pieter@appeltans.eu>
2017-03-26 10:02:29 +0200

    minor

--- matlab/fem.m
+++ matlab/fem.m
@@ -4,12 +4,18 @@
                                                    R2016a (9.0.0.341360) 64-bit (glnxa64)
                                                              February 11, 2016
 
- 
+
 For online documentation, see http://www.mathworks.com/support
 For product information, visit www.mathworks.com.
- 
+
 
 	Academic License
 
->> 
->> 
+>>
+>> [Warning:
+Connection to the X11 Display Server (:0) has been lost.
+No more graphics windows can no longer be displayed in this session.
+Graphics Objects can still be printed, and all commands should still work.
+We recommend that you try to save your current session and exit.]
+
+>>
